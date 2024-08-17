612d996f6915889cbd1f6072f6f7e0f1c96a017c
sharafath ali <sharafathaliofficial@gmail.com>
2024-08-17 14:59:03 +0530

    global object, modules & require

--- lesson 1/test.js
+++ lesson 1/test.js
@@ -1,4 +1,16 @@
-const a = 99;
-const b = 71;
+const array = require(`./const`)
+const operatingSystem = require(`os`)
 
-console.log(a+b,"hh");
+console.log("global", array, operatingSystem.userInfo())
+
+const aa = global.setInterval(() => {
+  console.log("after 1 s")
+}, 1000);
+
+global.setTimeout(() => {
+  console.log("time out")
+  clearInterval(aa)
+}, 3000);
+
+console.log(__dirname)
+console.log(__filename)
