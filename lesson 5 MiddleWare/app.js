f829913fdb87358db9982640bc49860aeda45b81
sharafath ali <sharafathaliofficial@gmail.com>
2024-08-21 21:41:07 +0530

    middleware class completed

---
+++ lesson 5 MiddleWare/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+var morgan = require('morgan') //third party middleware.
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+
+
+
+app.use((req, res , next) => {
+  console.log('Requested');
+  console.log('Host name:', req.hostname);
+  console.log('Path name:', req.path);
+  console.log('Method:', req.method);
+  next()
+});
+
+app.get('/', (req, res) => {
+  const data = {
+    title: 'Home Page',
+    user: { name: 'Sharafath Ali' },
+    items: ['coding', 'playing video games', 'learning new things']
+  };
+  res.render('index', data);
+});
+
+// About page route
+app.get('/about', (req, res) => {
+  const data = {
+    title: 'About Us',
+    user: { name: 'Sharafath Ali' }
+  };
+  res.render('about', data);
+});
+
+app.get("/create-Blog", (req, res) => {
+  res.render('createBlog')
+})
+
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
